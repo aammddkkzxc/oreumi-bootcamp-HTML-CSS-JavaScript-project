56620f3880214863de5f8d3b8bfc4aa28737ab89
aammddkkzxc <ideal0427@naver.com>
2024-01-22 04:05:52 +0900

    feat: 스크롤 기능 구현

--- else.js
+++ else.js
@@ -5,10 +5,14 @@ scrollBtn.addEventListener('mouseenter', function() {
 });
 
 function scrollUp() {
-    const scrollTime = 1000; // 스크롤이 완료되기까지의 시간 (밀리초)
+    const scrollTime = 1000;
     const scrollLength = -window.scrollY / (scrollTime / 10);
 
-    if (window.scrollY !== 0) {
-        window.scrollBy(0, scrollLength);
-    }
+    const scrollInterval = setInterval(() => {
+        if (window.scrollY !== 0) {
+            window.scrollBy(0, scrollLength);
+        } else {
+            clearInterval(scrollInterval);
+        }
+    }, 10);
 }
