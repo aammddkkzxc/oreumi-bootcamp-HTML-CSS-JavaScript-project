24e935e318afb56fb5e758970b84cfa379d9b5a3
aammddkkzxc <ideal0427@naver.com>
2024-01-22 14:34:49 +0900

    feat: 모달 띄우고 닫기 기능

--- else.js
+++ else.js
@@ -1,5 +1,8 @@
 const scrollBtn = document.getElementById('scroll_btn');
 let scrollInterval;
+const subscribeBtn = document.getElementById('subscribe_container_submit');
+const modalBody = document.querySelector('#modal_scope');
+const modalBtn = document.getElementById('modal_btn');
 
 scrollBtn.addEventListener('mouseenter', function() {
     scrollUp();
@@ -20,4 +23,39 @@ function scrollUp() {
             clearInterval(scrollInterval);
         }
     }, 15);
+}
+
+
+
+function openModal() {
+    modalBody.style.display='flex';
+}
+
+modalBtn.addEventListener('click', function () {
+    closeModal();
+})
+
+function closeModal() {
+    modalBody.style.display='none';
+}
+
+
+function emailCheck(email_address){
+    email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+    if(!email_regex.test(email_address)){
+        return false;
+    }else{
+        return true;
+    }
+}
+function validateEmail() {
+    let emailInput = document.getElementById('subscribe_container_input');
+
+    let email = emailInput.value;
+
+    if (emailCheck(email)) {
+        alert('유효한 이메일 주소입니다.');
+    } else {
+        alert('유효하지 않은 이메일 주소입니다.');
+    }
 }
--- imagescroll.js
+++ imagescroll.js
@@ -1,20 +1,16 @@
 const imageContainer = document.querySelector("#article_magazine_container");
 const showMoreBtn = document.getElementById('show_more_btn');
-let isFetching = false;
 let pageToFetch = 1;
 
 async function fetchImages(pageNum){
     try {
-        const response = await fetch('https://picsum.photos/v2/list?page='+pageNum+'&limit=6'); // 페이지, 리미트 설정
+        const response = await fetch('https://picsum.photos/v2/list?page='+pageNum+'&limit=3'); // 페이지, 리미트 설정
         if (!response.ok) {
             throw new Error('네트워크 응답에 문제가 있습니다.');
         }
-
         const datas = await response.json();
         console.log(datas);
-
         addImage(datas);
-
     } catch (error) {
         console.error('데이터를 가져오는데 문제가 발생했습니다 :', error);
     }
