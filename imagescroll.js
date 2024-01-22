6b961f2b56d10126a9e613c4ad8513f742ba01ae
aammddkkzxc <ideal0427@naver.com>
2024-01-22 22:13:43 +0900

    refactor: 구분선 기준 요소 변경

--- imagescroll.js
+++ imagescroll.js
@@ -21,23 +21,18 @@ function addImage(datas) {
         imageContainer.innerHTML = imageContainer.innerHTML + "<img src="+ item.download_url +" alt=' '>";
     });
     //.innerHTML = "<li><img src=" +datas.[0].download.url + " alt=' '><li>";
-}// 백틱 문자열 쓰면 더 간단
-// 넘어오는 datas수만큼 반복문 => forEach
-window.addEventListener('scroll', ()=>{
-    //스크롤이 상단으로부터 얼마나 이동했는지 알아야합니다. (뷰포트의 높이 + 스크롤된 길이)
-    //화면에 로딩된 페이지의 전체 높이
-    //뷰포트의 높이 + 스크롤된 길이 + 5~10px = 화면에 로딩된 페이지의 전체 높이
-    if (!isFetching && window.innerHeight + document.documentElement.scrollTop + 10 >= document.documentElement.offsetHeight) {
-        isFetching = true;
-        fetchImages(pageToFetch += 1).finally(() => {
-            isFetching = false;
-        });
-    }
-    //offsetHeingt => 패딩과 테두리를 다 포함한
-})
-//scroll 무디게 만들어 줘야 한다 힌트 쓰로틀링
-// 이미지가 너무 많이 로드됨
+}
 
 showMoreBtn.addEventListener('click', () => {
     fetchImages(pageToFetch += 1);
+});
+
+imageContainer.addEventListener('scroll', function () {
+    // 스크롤 위치 확인
+    const isScrolledToBottom = imageContainer.scrollHeight - imageContainer.clientHeight <= imageContainer.scrollTop + 1;
+
+    // 스크롤이 맨 아래에 도달하면 새로운 데이터 로드
+    if (isScrolledToBottom) {
+        fetchImages(pageToFetch += 1)
+    }
 });
